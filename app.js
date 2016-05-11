62364b7f1bb647151d1b60f47786730b53e9b928
Sriharsha Karampudi <karampudi3@gmail.com>
2016-05-10 22:07:22 -0500

    1.1.2

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-var app = angular.module('gemStore', []);
+
+(function(){
+	var app = angular.module('Store', [ ]);
+
+	app.controller('StoreController', function() {
+		this.products =gems;
+	});
+
+
+	var gems = [ 
+
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Product 1',
+		canPurchase: true,
+		soldOut: true
+	},
+
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Product 2',
+		canPurchase: false,
+	}
+	];
+
+})();
